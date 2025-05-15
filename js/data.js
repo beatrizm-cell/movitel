45e65ce04d2495a79c1ff848395f6859f39befd5
beatrizm-cell <beatrizmanhica05@gmail.com>
2025-05-15 23:00:41 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,37 @@
+const quizData = [
+  {
+    question: 'Em que ano a Movitel foi fundada?',
+    image: 'images/Movitel-Logo.jpg',
+    options: ['2005', '2008', '2011', '2015']
+  },
+  {
+    question: 'Quantas províncias de Moçambique são atendidas pelos serviços da Movitel?',
+    image: 'images/pais.png',
+    options: ['Apenas 5 províncias', '8 províncias', 'Todas as 11 províncias', 'Apenas 2 províncias']
+  },
+  {
+    question: 'Qual é o slogan atual da Movitel?',
+    image: 'images/todo.jpg',
+    options: ['Sempre em Movimento', 'A rede que te conecta', 'Ligando Moçambique', 'Em todo lugar a todo momento']
+  },
+  {
+    question: 'Quais destes serviços são oferecidos pela Movitel?',
+    image: 'images/servicos.jpg',
+    options: ['m-Pesa', 'm-Kesh', 'emola', 'soprati']
+  },
+  {
+    question: 'Quem são os principais concorrentes da Movitel no mercado moçambicano?',
+    image: 'images/concorrentes.jpg',
+    options: ['Tmcel e Vodacom', 'Orange e Airtel', 'Mcel e Unitel', 'Tim e Unitel']
+  },
+  {
+    question: 'Que tipo de tecnologia a Movitel oferece em seus serviços?',
+    image: 'images/4g.jpg',
+    options: ['Apenas 3G', '4G e fibra óptica', '2G e rede de satélite', 'Wi-Fi gratuito']
+  },
+  {
+    question: 'Você recomendaria a sua família a usar a Movitel?',
+    image: 'images/familia.jpg',
+    options: ['Sim', 'Não']
+  }
+];
